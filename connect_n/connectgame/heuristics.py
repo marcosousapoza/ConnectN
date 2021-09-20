162b9996621b5806f1447ee84b27337a73d6399d
marco_sousapoza <marco@sousa-poza.net>
2021-09-20 09:06:58 +0200

    finishing touch

--- connect_n/connectgame/heuristics.py
+++ connect_n/connectgame/heuristics.py
@@ -1,5 +1,20 @@
 from . import Heuristic, Board, Game
 
+
+class BadHeuristic(Heuristic):
+
+    def __init__(self, game_n:int) -> None:
+        super().__init__(game_n)
+    
+    def _name(self) -> str:
+        return "Bad"
+    
+    def evaluate(self, player:int, board:Board) -> int:
+        """Determine utility of a board state
+        """
+        return 0
+
+
 class SimpleHeuristic(Heuristic):
 
     def __init__(self, game_n:int): 
@@ -11,13 +26,14 @@ class SimpleHeuristic(Heuristic):
     def evaluate(self, player:int, board:Board) -> int:
         """Determine utility of a board state
         """
+
+        # If winning or losing evaluation is easy
         board_state = board.get_board_state()
         win = Game.winning(board_state, self.game_n)
         if(win == player):
             return float('inf')
         elif(win != 0):
             return -float('inf')
-        
 
         # If not winning or losing, return highest number of claimed squares in a row         
         max_inRow = 0
@@ -30,25 +46,32 @@ class SimpleHeuristic(Heuristic):
                             max_inRow = max(max_inRow, x + 1)
                         else:
                             break
-                    
                     for y in range(1, len(board_state[0]) - j):
                         if (board_state[i][j + y] == player):
                             max_inRow = max(max_inRow, y + 1)
                         else:
                             break
-                        
-                    
                     for d in range(1, min(len(board_state) - i, len(board_state[0]) - j)): 
                         if board_state[i + d][j + d] == player:
                             max_inRow = max(max_inRow, d + 1)
                         else: 
                             break
-                        
-                    
                     for a in range(1, min(len(board_state) - i, j)):
                         if board_state[i + a][j - a] == player:
                             max_inRow = max(max_inRow, a + 1)
                         else:
                             break
-
         return max_inRow
+
+
+class BetterHeuristic(Heuristic):
+
+    def __init__(self, game_n:int) -> None:
+        super().__init__(game_n)
+
+    def _name(self) -> str:
+        return "Better"
+
+    def evaluate(self, player: int, board: Board) -> int:
+        # TODO write heuristic
+        return 0
